37029f7ee9184cca74da0b46f07f566b0c97de68
majid-tech <majidmohammed044@gmail.com>
2026-02-27 12:42:49 +0000

    Import Library

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Book, Library
-
+from .models import Library
+from .models import Book
 
 # Function-based view
 def book_list(request):
