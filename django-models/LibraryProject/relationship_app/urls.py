bebb9c493548af10dba982c9339925c7ab8d33c4
majid-tech <majidmohammed044@gmail.com>
2026-03-01 13:37:32 +0000

    alter book option

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -6,9 +6,9 @@ from . import views
 
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),
-    path('books/add/', views.add_book, name='add_book'),
-    path('books/<int:pk>/edit/', views.edit_book, name='edit_book'),
-    path('books/<int:pk>/delete/', views.delete_book, name='delete_book'),
+    path('add_book/', views.add_book, name='add_book'),
+    path('edit_book/<int:pk>/', views.edit_book, name='edit_book'),
+    path('delete_book/<int:pk>/', views.delete_book, name='delete_book'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
     path(
         'login/',
