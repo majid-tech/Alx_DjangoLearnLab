b8116775b048bfa99deee8362a6b947c3f4f54a0
majid-tech <majidmohammed044@gmail.com>
2026-03-01 13:22:57 +0000

    authenticate uzer

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,20 +1,21 @@
-from django.urls import path
-from .views import LibraryDetailView
-from .views import list_books, register
 from django.contrib.auth import views as auth_views
-from .views import register
+from django.urls import path
+
+from .views import LibraryDetailView, list_books, register
 
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    path('login/', auth_views.LoginView.as_view(
-        template_name='relationship_app/login.html'
-    ), name='login'),
-
-    path('logout/', auth_views.LogoutView.as_view(
-        template_name='relationship_app/logout.html'
-    ), name='logout'),
-
+    path(
+        'login/',
+        auth_views.LoginView.as_view(template_name='relationship_app/login.html'),
+        name='login',
+    ),
+    path(
+        'logout/',
+        auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'),
+        name='logout',
+    ),
     path('register/', register, name='register'),
 ]
