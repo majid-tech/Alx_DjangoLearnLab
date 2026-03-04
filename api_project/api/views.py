8fd7f2e18c769a6595f2c204601828dc219d5b04
majid-tech <majidmohammed044@gmail.com>
2026-03-04 23:22:23 +0000

    Rectify/Configure api/url.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,14 +2,8 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
-from .views import BookList
-from django.urls import path
 # Create your views here.
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-
-urlpatterns = [
-    path('books/', BookList.as_view(), name = 'book-list'), # Maps to the BookList view
-]
